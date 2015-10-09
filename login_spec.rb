b41aa234a461692a2daaeeb1b6b171547cb82a2c
Joey Hilton <joeyhilton@Joey-Hilton.local>
2015-10-09 12:06:39 -0600

    finished chap 8

--- login_spec.rb
+++ login_spec.rb
@@ -1,4 +1,5 @@
 require 'selenium-webdriver'
+require_relative 'login'
 
 RSpec.configure do |config|
   config.expect_with :rspec do |c|
@@ -7,21 +8,21 @@ RSpec.configure do |config|
 end
 
 describe 'Login' do
-
-	before(:each) do 
+	before(:each) do
 		@driver = Selenium::WebDriver.for :firefox
+		ENV['base_url'] = 'http://the-internet.herokuapp.com'
+		@login = Login.new(@driver)
 	end
-	
 	after(:each) do
-		@driver.quit 
+		@driver.quit
+	end
+	it 'succeeded' do
+		@login.with('tomsmith', 'SuperSecretPassword!')
+		@login.success_message_present?.should be true
 	end
 
-	it 'succeeded' do 
-		@driver.get 'http://the-internet.herokuapp.com/login'
-		@driver.find_element(id: 'username').send_keys('tomsmith')
-		@driver.find_element(id: 'password').send_keys('SuperSecretPassword!')
-		@driver.find_element(id: 'login').submit
-		@driver.find_element(css: '.flash.success').displayed?.should be true
+	it 'failed' do
+	@login.with('asdf', 'asdf')
+	@login.failure_message_present?.should be true
 	end
-
 end
