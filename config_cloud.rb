c7cf2fcf513c86a50da775cd2f526aee36995754
Joey Hilton <joey@masteryconnect.com>
2015-10-13 09:47:16 -0600

    added config cloud file

---
+++ config_cloud.rb
@@ -0,0 +1,7 @@
+ENV['base_url'] ||= 'http://the-internet.herokuapp.com'
+ENV['host'] = 'saucelabs'
+ENV['operating_system'] ||= 'Windows XP'
+ENV['browser'] ||= 'internet_explorer'
+ENV['browser_version'] ||= '8'
+ENV['SAUCE_USERNAME'] = 'shnek81'
+ENV['SAUCE_ACCESS_KEY'] = '7f605afa-d061-4937-b035-6125107af4e3'
